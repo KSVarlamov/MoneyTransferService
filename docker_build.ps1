e92c5f0be018e16fcdad0646111657fb1b5fa01b
KSVarlamov <varlamov.ks@mail.ru>
2023-05-14 19:14:55 +0300

    Обновление номера версии

--- docker_build.ps1
+++ docker_build.ps1
@@ -1,3 +1,3 @@
 .\mvnw clean package -DskipTests
 
-docker build -t money-transfer-service:latest -t money-transfer-service:0.0.2 -f ./docker/Dockerfile .
+docker build -t money-transfer-service:latest -t money-transfer-service:0.0.3 -f ./docker/Dockerfile .
