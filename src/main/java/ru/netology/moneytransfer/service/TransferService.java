055c99273c4893841b40db5558a2e43b8e858a17
KSVarlamov <varlamov.ks@mail.ru>
2023-05-14 19:50:56 +0300

    Исправление замечания 11. Проверки операции в отельный класс

--- src/main/java/ru/netology/moneytransfer/service/TransferService.java
+++ src/main/java/ru/netology/moneytransfer/service/TransferService.java
@@ -2,13 +2,11 @@ package ru.netology.moneytransfer.service;
 
 import org.springframework.stereotype.Service;
 import ru.netology.moneytransfer.dto.CardToCardOperationDTO;
-import ru.netology.moneytransfer.exceptions.CardNotValidException;
-import ru.netology.moneytransfer.exceptions.OperationException;
 import ru.netology.moneytransfer.model.CardToCardOperation;
 import ru.netology.moneytransfer.repository.OperationsRepository;
+import ru.netology.moneytransfer.utils.CardToCardOperationChecker;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
@@ -30,45 +28,9 @@ public class TransferService {
         operation.setAmount(amountInRub);
         operation.setCommission(amountInRub.multiply(COMMISSION_PRICE));
 
-        checkOperation(operationDTO, operation);
+        CardToCardOperationChecker.checkOperation(operationDTO, operation);
         operation.setStatus(CardToCardOperation.Status.WAITING_FOR_CONFIRM);
 
         return operation;
     }
-
-    private void checkOperation(CardToCardOperationDTO operationDTO, CardToCardOperation operation) {
-        if (operationDTO.cardFromNumber().equals(operationDTO.cardToNumber())) {
-            String err = String.format("Ошибка обработки операции: Нельзя отправить деньги самому себе [%s]", operationDTO.cardFromNumber());
-            operation.setStatus(CardToCardOperation.Status.FAILED);
-            operation.setReason(err);
-            throw new OperationException(err, operation);
-        } else {
-            operation.setCcFrom(operationDTO.cardFromNumber());
-            operation.setCcTo(operationDTO.cardToNumber());
-        }
-        operation.setCurrency(operationDTO.amount().currency());
-        if (!"RUR".equals(operationDTO.amount().currency())) {
-            String err = "Ошибка обработки операции: Доступны переводы только в рублях";
-            operation.setStatus(CardToCardOperation.Status.FAILED);
-            operation.setReason(err);
-            throw new OperationException(err, operation);
-        }
-        String[] parts = operationDTO.cardFromValidTill().split("/");
-        int month = Integer.parseInt(parts[0]);
-        if (month > 12 || month <= 0) {
-            String err = String.format("Ошибка обработки операции: Некорректная дата действия карты [%s]. Месяц в диапазоне должен быть 01..12", operationDTO.cardFromValidTill());
-            operation.setStatus(CardToCardOperation.Status.FAILED);
-            operation.setReason(err);
-            throw new CardNotValidException(err, operation);
-        }
-        LocalDate date = LocalDate.now();
-        int cardYear = Integer.parseInt(parts[1]) + 2000;
-        if ((cardYear < date.getYear()) || ((cardYear == date.getYear()) && (month < date.getMonthValue()))) {
-            String err = String.format("Ошибка обработки операции: Дата действия карты [%s] истекла", operationDTO.cardFromValidTill());
-            operation.setStatus(CardToCardOperation.Status.FAILED);
-            operation.setReason(err);
-            throw new CardNotValidException(err, operation);
-        }
-    }
-
 }
---
+++ src/main/java/ru/netology/moneytransfer/utils/CardToCardOperationChecker.java
@@ -0,0 +1,50 @@
+package ru.netology.moneytransfer.utils;
+
+import ru.netology.moneytransfer.dto.CardToCardOperationDTO;
+import ru.netology.moneytransfer.exceptions.CardNotValidException;
+import ru.netology.moneytransfer.exceptions.OperationException;
+import ru.netology.moneytransfer.model.CardToCardOperation;
+
+import java.time.LocalDate;
+
+public class CardToCardOperationChecker {
+
+    private CardToCardOperationChecker() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void checkOperation(CardToCardOperationDTO operationDTO, CardToCardOperation operation) {
+        if (operationDTO.cardFromNumber().equals(operationDTO.cardToNumber())) {
+            String err = String.format("Ошибка обработки операции: Нельзя отправить деньги самому себе [%s]", operationDTO.cardFromNumber());
+            operation.setStatus(CardToCardOperation.Status.FAILED);
+            operation.setReason(err);
+            throw new OperationException(err, operation);
+        } else {
+            operation.setCcFrom(operationDTO.cardFromNumber());
+            operation.setCcTo(operationDTO.cardToNumber());
+        }
+        operation.setCurrency(operationDTO.amount().currency());
+        if (!"RUR".equals(operationDTO.amount().currency())) {
+            String err = "Ошибка обработки операции: Доступны переводы только в рублях";
+            operation.setStatus(CardToCardOperation.Status.FAILED);
+            operation.setReason(err);
+            throw new OperationException(err, operation);
+        }
+        String[] parts = operationDTO.cardFromValidTill().split("/");
+        int month = Integer.parseInt(parts[0]);
+        if (month > 12 || month <= 0) {
+            String err = String.format("Ошибка обработки операции: Некорректная дата действия карты [%s]. Месяц в диапазоне должен быть 01..12", operationDTO.cardFromValidTill());
+            operation.setStatus(CardToCardOperation.Status.FAILED);
+            operation.setReason(err);
+            throw new CardNotValidException(err, operation);
+        }
+        LocalDate date = LocalDate.now();
+        int cardYear = Integer.parseInt(parts[1]) + 2000;
+        if ((cardYear < date.getYear()) || ((cardYear == date.getYear()) && (month < date.getMonthValue()))) {
+            String err = String.format("Ошибка обработки операции: Дата действия карты [%s] истекла", operationDTO.cardFromValidTill());
+            operation.setStatus(CardToCardOperation.Status.FAILED);
+            operation.setReason(err);
+            throw new CardNotValidException(err, operation);
+        }
+    }
+}
