fe22cb1e2d0399d6c292a2f53973ad9cdccc025a
KSVarlamov <varlamov.ks@mail.ru>
2023-05-15 08:42:50 +0300

    Валидация на уровне репозитория

--- src/main/java/ru/netology/moneytransfer/repository/OperationsRepositoryInMemory.java
+++ src/main/java/ru/netology/moneytransfer/repository/OperationsRepositoryInMemory.java
@@ -1,6 +1,9 @@
 package ru.netology.moneytransfer.repository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 import ru.netology.moneytransfer.model.CardToCardOperation;
 
 import java.util.Map;
@@ -8,16 +11,17 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
+@Validated
 public class OperationsRepositoryInMemory implements OperationsRepository {
     private final Map<Integer, CardToCardOperation> operations = new ConcurrentHashMap<>();
 
     @Override
-    public void add(CardToCardOperation operation) {
+    public void add(@NotNull @Valid CardToCardOperation operation) {
         operations.put(operation.getId(), operation);
     }
 
     @Override
-    public boolean delete(CardToCardOperation operation) {
+    public boolean delete(@NotNull CardToCardOperation operation) {
         return operations.remove(operation.getId(), operation);
     }
 
